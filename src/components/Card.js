70e3c73b375195dd90829396d5d76d52aafe607b
EC2 Default User <ec2-user@ip-172-31-47-35.ap-northeast-1.compute.internal>
2019-12-23 10:38:30 +0000

    card chenge input p

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,26 @@
 import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
 const Card = () => {
-  const [content, setContent] = new useState('')
-  const dblclick = () => {
-    let parent = this.parentNode
-    parent.replaceChild(input, p)
-    input.focus()
+  const [content, setContent] = new useState('aaa')
+  const [mode, setMode] = new useState('p')
+  const changedContentHandler = (e) => setContent(e.target.value)
+  var element = null
+  switch (mode) {
+    case 'p':
+      element = <p onDoubleClick={ () => setMode('input') }>{content}</p>
+      break;
+    case 'input':
+      element = <input type="text" autoFocus="focus" value={content}
+                onChange={ changedContentHandler }
+                onKeyDown={ e => { if(e.keyCode === 13) setMode('p') }} 
+                onBlur={ () => setMode('p') }
+                />
+      break;
   }
-  const keyDown = (e) => {
-    if(e.keyCode === 13){
-      let parent = this.parentNode
-      setContent(this.value)
-      parent.replaceChild(p, this)
-    }
-  }
-  const blur = () => {
-    let parent = this.parentNode
-    setContent(this.value)
-    parent.replaceChild(p, this)
-  }
-  const input = <input type="text" value={content} onkeydown={keyDown()} onblur={blur()}/>
-  const p = <p ondblclick={dblclick()}>{content}</p>
   return (
-    <div>
-      {p}
+    <div className="cardItem">
+      {element}
     </div>
     )
 }
